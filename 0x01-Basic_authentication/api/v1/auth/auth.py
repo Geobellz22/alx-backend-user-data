7fc60de22698a93d3e6acb37143ddbd8eaa772d7
Geobellz22 <st377126@gmail.com>
2023-11-11 12:54:01 +0000

    Task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,15 @@ from typing import List, TypeVar
 class Auth:
     """Method to check if authentication is required"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns False"""
-        return False
+        """Require auth method"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if len(path) == 14:
+            path = f"{path}/"
+
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """Returns None"""
