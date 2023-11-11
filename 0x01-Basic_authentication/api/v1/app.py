e5f6a8df8bbe7b6e3ea72291b4409fefae3ac6ba
Geobellz22 <st377126@gmail.com>
2023-11-11 13:59:40 +0000

    app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-
 """
 Route module for the API
 """
-
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, request, abort
-from flask_cors import CORS
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
 
 AUTH_TYPE = getenv("AUTH_TYPE")
@@ -28,10 +27,10 @@ def before_request():
         pass
     else:
         excluded_paths = [
-            'api/v1/status/',
-            'api/v1/unauthorized/',
-            'api/v1/forbidden/'
-        ]
+                            '/api/v1/status/',
+                            '/api/v1/unauthorized/',
+                            '/api/v1/forbidden/'
+                        ]
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
@@ -41,23 +40,24 @@ def before_request():
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler"""
+    """unauthorized"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """Forbidden handler"""
+def Forbidden(error) -> str:
+    """Forbidden"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = int(getenv("API_PORT", "5000"))  # Ensure port is an integer
-    app.run(host=host, port=port)
+    port = getenv("API_PORT", "5000")
+    app.run(host, port)
