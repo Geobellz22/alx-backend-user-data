38044e4c311b7576f6fe3272807db66de806daf1
Geobellz22 <st377126@gmail.com>
2023-11-11 12:27:48 +0000

    Task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
-from flask import request
-from typing import List, TypeVar
+"""class Auth to manage API"""
 
+from typing import List, TypeVar
+from flask import request
 
 class Auth:
-    """Auth class"""
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Method to check if authentication is required"""
-        if path is None or excluded_paths is None or excluded_paths == []:
-            return False
-
+        """Returns False"""
+        return False
 
     def authorization_header(self, request=None) -> str:
-        """Method to get the authorization header"""
-        if request is None or "Authorization" not in request.headers:
-            return None
-        else:
-            return request.headers.get("Authorization")
+        """Returns None"""
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Method to get the current user"""
+        """Returns None"""
         return None
