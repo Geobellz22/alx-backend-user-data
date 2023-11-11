37b873a831e2d176357d10da75b863319b417c57
Geobellz22 <st377126@gmail.com>
2023-11-11 13:49:31 +0000

    app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,9 +16,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE:  # Use truthiness to check if AUTH_TYPE is not empty
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+
 
 @app.before_request
 def before_request():
@@ -31,31 +32,30 @@ def before_request():
             'api/v1/unauthorized/',
             'api/v1/forbidden/'
         ]
-
-        # Skip authorization checks for /api/v1/status
-        if request.path == '/api/v1/status/':
-            return
-
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
 
+
 @app.errorhandler(404)
-def not_found(error):
+def not_found(error) -> str:
     """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
-def unauthorized(error):
+def unauthorized(error) -> str:
     """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
-def forbidden(error):
+def forbidden(error) -> str:
     """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
