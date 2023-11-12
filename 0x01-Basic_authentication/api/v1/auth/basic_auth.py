1122b2c34842906580d4cbdd66ce314bde362ca2
Geobellz22 <st377126@gmail.com>
2023-11-12 22:35:50 +0000

    Task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,3 +20,16 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[-1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decode Base64 Authorization header"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            value = base64_authorization_header.encode('utf-8')
+            decoded_value = base64.b64decode(value)
+        except Exception:
+            return None
