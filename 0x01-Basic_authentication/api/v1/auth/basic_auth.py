bfa74b666faf8e787f4dc53c4f5d134f555da982
Geobellz22 <st377126@gmail.com>
2023-11-12 23:41:21 +0000

    Task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,7 @@ Basic Auth Class
 from api.v1.auth.auth import Auth
 import base64
 from models.user import User
-from typing import TypeVar
+
 
 class BasicAuth(Auth):
     """BasicAuth class"""
@@ -48,29 +48,22 @@ class BasicAuth(Auth):
         user_email, password = decoded_base64_authorization_header.split(":")
         return (user_email, password)
 
-    def user_object_from_credentials(
-        self, user_email: str, user_pwd: str) -> User:
-    """Get User instance based on email and password"""
-    if user_email is None or type(user_email) is not str:
-        return None
-    if user_pwd is None or type(user_pwd) is not str:
-        return None
-    try:
-        users = User.search({"email": user_email})
-        if len(users) == 0:
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """user_object_from_credentials method"""
+        if user_email is None or type(user_email) is not str:
             return None
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user  # Return the found user, not the entire list
-        return None
-    except Exception:
-        return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """current_user method"""
-        auth_header = self.authorization_header(request)
-        auth_token = self.extract_base64_authorization_header(auth_header)
-        decoded = self.decode_base64_authorization_header(auth_token)
-        email, passwd = self.extract_user_credentials(decoded)
-        user = self.user_object_from_credentials(email, passwd)
-        return user
+        try:
+            users = User.search({"email": user_email})
+            if len(users) == 0:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
