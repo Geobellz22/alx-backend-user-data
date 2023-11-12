1f4562a58da389da6b06a25d06411e0355274756
Geobellz22 <st377126@gmail.com>
2023-11-12 21:58:10 +0000

    Task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,17 @@
 """Basic Auth Class"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Basic auth class"""
-    pass
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """extract_base64_authorization_header"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startwith("Basic"):
+            return None
