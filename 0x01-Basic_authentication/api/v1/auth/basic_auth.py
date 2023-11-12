8c11607dc7ed1c8488e24746831bb6547ee6c2fc
Geobellz22 <st377126@gmail.com>
2023-11-12 23:44:28 +0000

    Task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,3 +68,12 @@ class BasicAuth(Auth):
             return None
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user method"""
+        auth_header = self.authorization_header(request)
+        auth_token = self.extract_base64_authorization_header(auth_header)
+        decoded = self.decode_base64_authorization_header(auth_token)
+        email, passwd = self.extract_user_credentials(decoded)
+        user = self.user_object_from_credentials(email, passwd)
+        return user
