8e8fec2e94841943e4efd96ffa07c8891febb399
Geobellz22 <st377126@gmail.com>
2023-11-16 21:48:24 +0000

    Users file

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,7 +30,7 @@ if AUTH_TYPE == "session_auth":
 def before_request():
     """Runs before request"""
     if auth is None:
-        pass
+        return
     else:
         excluded_paths = [
                             '/api/v1/status/',
@@ -40,11 +40,12 @@ def before_request():
         if auth.require_auth(request.path,  excluded_paths) is False:
             return
 
-         if ((auth.authorization_header(request) is None) and (auth.session_cookie(request) is None)):
+        if ((auth.authorization_header(request) is None) and
+                (auth.session_cookie(request) is None)):
             abort(401)
 
-             if (auth.current_user(request) is None):
-        abort(403)
+        if (auth.current_user(request) is None):
+            abort(403)
     request.current_user = auth.current_user(request)
 
 
