5b6a23c70b6dc08c163557821b068df52b543129
Geobellz22 <st377126@gmail.com>
2023-11-20 22:49:16 +0000

    Task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,6 +35,9 @@ def login() -> str:
     if not email or not password:
         abort(401)
 
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
     session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
