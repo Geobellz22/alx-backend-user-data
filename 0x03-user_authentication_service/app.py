bf37a84b016e5b1da55876b46a88ed7d45c01f2b
Geobellz22 <st377126@gmail.com>
2023-11-20 13:54:12 +0000

    app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python3
+"""Setting up a basic flask app"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index() -> str:
+    message = {"message": "Bienvenue"}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
