9ca75af418a05d48d9381343d1bd126ab6ff1716
Geobellz22 <st377126@gmail.com>
2023-11-20 22:23:06 +0000

    Task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,27 +9,25 @@ from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
-    """hash input password"""
+    """Hash input password."""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-
     return hashed_password
 
 
 def _generate_uuid() -> str:
-    """Generate and return a string representation of a new UUID"""
+    """Generate and return a string representation of a new UUID."""
     return str(uuid.uuid4())
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
-    """
+    """Auth class to interact with the authentication database."""
 
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """register a new user"""
+        """Register a new user."""
         try:
             self._db.find_user_by(email=email)
         except NoResultFound:
@@ -38,10 +36,22 @@ class Auth:
         raise ValueError(f"User {email} already exists")
 
     def valid_login(self, email: str, password: str) -> bool:
-        """ Validate login"""
+        """Validate login."""
         try:
             user = self._db.find_user_by(email=email)
             if user is not None:
-                return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)  # noqa E502
+                return bcrypt.checkpw(password.encode
+                                      ("utf-8"), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a new session for the user."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
+        except NoResultFound:
+            return None
