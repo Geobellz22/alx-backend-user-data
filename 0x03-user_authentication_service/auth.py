0555228ce2e20aeee0a5707c533129559d0cbc03
Geobellz22 <st377126@gmail.com>
2023-11-23 21:34:07 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,7 +57,7 @@ class Auth:
         except NoResultFound:
             return None
 
-     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Retrieves a user based on a given session ID.
         """
         user = None
