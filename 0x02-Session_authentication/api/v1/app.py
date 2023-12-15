573adfd55bd5afab2a122a844fa2b7069b7bfb98
Geobellz22 <st377126@gmail.com>
2023-12-15 22:37:12 +0000

    Task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Route module for the API.
+"""
+Route module for the API.
 """
 import os
 from os import getenv
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
@@ -13,12 +14,12 @@ from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
+
 if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
@@ -33,29 +34,25 @@ if auth_type == 'session_db_auth':
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler.
-    """
+    """Not found handler."""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler.
-    """
+    """Unauthorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler.
-    """
+    """Forbidden handler."""
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def authenticate_user():
-    """Authenticates a user before processing a request.
-    """
+    """Authenticates a user before processing a request."""
     if auth:
         excluded_paths = [
             "/api/v1/status/",
@@ -63,8 +60,9 @@ def authenticate_user():
             "/api/v1/forbidden/",
             "/api/v1/auth_session/login/",
         ]
-         if auth.require_auth(request.path, excluded_paths) is False:
-        return
+
+        if auth.require_auth(request.path, excluded_paths) is False:
+            return
 
     if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
