7bf13c5cc364c44a9987ffe251ccaae70004c1e8
Geobellz22 <st377126@gmail.com>
2023-12-15 22:19:21 +0000

    Task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,14 +63,16 @@ def authenticate_user():
             "/api/v1/forbidden/",
             "/api/v1/auth_session/login/",
         ]
-        if auth.require_auth(request.path, excluded_paths):
-            user = auth.current_user(request)
-            if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
-                abort(401)
-            if user is None:
-                abort(403)
-            request.current_user = user
+         if auth.require_auth(request.path, excluded_paths) is False:
+        return
+
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
